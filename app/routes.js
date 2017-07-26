1fa60b317cef39e27f879c87fc43cdfdcdc3312f
System <none>
2017-07-26 22:38:37 +0000

    Checkpoint

--- app/routes.js
+++ app/routes.js
@@ -23,7 +23,7 @@ module.exports = function(app){
     var long = "https://"+req.params.url;
    
     
-    if(validUrl.isUri(long)){
+    if(validateURL(long)){
       
       
       
@@ -163,8 +163,32 @@ module.exports = function(app){
   
   
   app.route("/:num").get(function(req, res){
+    var num = req.params.num;
+     MongoClient.connect(url, function (err, db) {
+        if (err) {
+          res.send("Error connecting");
+          console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+          var collection = db.collection("short-url");
+          collection.find({"short-url": "https://wg-url-shortener.glitch.me/"+num}).toArray(function(err, doc){
+            if(doc.length==0){
+              res.send("Not a v")
+            }
+          })
+        }
+       
+     });
     
   });
+  
+  
+  function validateURL(url) {
+    // Checks to see if it is an actual url
+    // Regex from https://gist.github.com/dperini/729294
+    var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+    return regex.test(url);
+  }
+  
   
   
 };
