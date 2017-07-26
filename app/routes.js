0a09a66423d3a1a1ff99ed5edb30722bd0f25134
System <none>
2017-07-26 19:32:11 +0000

    Checkpoint

--- app/routes.js
+++ app/routes.js
@@ -5,13 +5,20 @@ module.exports = function(app){
   
   
   app.route("/").get(function(req, res){
-    
+    res.sendFile(process.cwd() + '/views/index.html');
   });
   
   
   
   
-  
+  app.route("/:url").get(function(req, res){
+    
+    //Validate URL; If good proceed
+    var query = req.params.query
+    if(validUrl)
+    
+    
+  });
   
   
   
