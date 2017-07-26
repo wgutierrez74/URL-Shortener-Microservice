7a300f971fe646fe5a83f841f744f92740b32566
System <none>
2017-07-26 22:16:36 +0000

    Checkpoint

--- app/routes.js
+++ app/routes.js
@@ -88,7 +88,83 @@ module.exports = function(app){
   });
   
   
-  app.route("/:num").get(func)
+  
+  app.route("/http://:url").get(function(req, res){
+    
+    //Validate URL; If good proceed
+    var long = "http://"+req.params.url;
+   
+    
+    if(validUrl.isUri(long)){
+      
+      
+      
+      MongoClient.connect(url, function (err, db) {
+        if (err) {
+          res.send("Error connecting");
+          console.log('Unable to connect to the mongoDB server. Error:', err);
+        } else {
+           //res.send("connected1");
+            var collection = db.collection('short-urls');
+            
+            var cursor = collection.find({"real-url": long})
+            cursor.toArray(function(err, docs){
+              var short;
+              if(docs.length == 0){
+               //Add long to db, create short, insert into db return short
+               //res.send("connected2");
+              collection.count({}, function(err, count){
+                short="https://wg-url-shortener.glitch.me/"+(count+1);
+                collection.insert({"real-url": long, "short-url": short});
+                var json = {
+              "Original URL": long,
+              "Short URL": short
+              };
+            res.send(json);
+            db.close();
+              });
+              //db.insert({"real-url": long, "short-url": "https://wg-url-shortener.glitch.me/"+addon});
+              //short="https://wg-url-shortener.glitch.me/"+addon;
+              
+            }
+            else{
+              short = docs[0]["short-url"];
+              var json = {
+              "Original URL": long,
+              "Short URL": short
+              };
+            res.send(json);
+            db.close();
+             
+            }
+            
+            
+            
+              
+              
+            });
+      }
+  
+    });
+      
+      
+  
+      
+    }
+    else{
+      
+      res.send("Not a valid url")
+      
+    }
+    
+  });
+  
+  
+  
+  
+  app.route("/:num").get(function(req, res){
+    
+  });
   
   
 };
